6a54ae82734615d841dc41659c10001dda202358
jhseob96 <jhseob96@gmail.com>
2024-12-18 22:09:18 +0900

    fix: router problem

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -3,20 +3,16 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { routes } from '../config/routes';
 import { ContainerLoading } from '@/shared';
 
-import type { Route as RouteType } from '../config/routes';
-
 export const AppRouter = () => (
   <Suspense fallback={<ContainerLoading />}>
     <Routes>
-      {routes.map((item, index) => (
-        <RouteWrapper key={index} {...item} />
-      ))}
+      {routes.map((item, index) => {
+        if (item.path === '/*') {
+          return <Route key={index} path="/*" element={<Navigate to="/" replace />} />;
+        }
+        const { path, element, ...rest } = item;
+        return <Route key={index} path={path} element={element} {...rest} />;
+      })}
     </Routes>
   </Suspense>
 );
-
-const RouteWrapper = (props: RouteType) => {
-  const { element, path, ...rest } = props;
-
-  return <Route index={true} path={path} element={path === '/*' ? <Navigate to="/" replace /> : element} {...rest} />;
-};
