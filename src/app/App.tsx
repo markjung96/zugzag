55ce9b482467dc16280bb32f5b922c0e20d2e4ac
Mark_Jeong <81726531+markjung96@users.noreply.github.com>
2025-01-05 18:04:14 +0900

    feat: rm layout provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { AppRouter } from './router';
-import { LayoutProvider, ThemeProvider } from './provider';
+import { ThemeProvider } from './provider';
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <LayoutProvider>
-          <AppRouter />
-        </LayoutProvider>
+        <AppRouter />
       </ThemeProvider>
     </div>
   );
